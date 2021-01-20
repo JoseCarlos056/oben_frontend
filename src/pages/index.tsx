25f30468fb861f53f4c8a668ea4ac5db72940e4e
JoseCarlos059 <josecarlos.depaula17@gmail.com>
2021-01-20 20:59:05 -0300

    ajustando estilo da tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,54 +16,60 @@ const Home: React.FC = () => {
           <img src={vetorlogin} alt="" />
         </div>
         <div className="auth-panel">
-          <div className="change-input">
-            <p>Login</p>
-            <div></div>
-            <p>Cadastro</p>
-          </div>
-          <h1>Bem vindo de volta!</h1>
-          <h2>Login</h2>
-          <div className="input">
-            <p>
-              <User />
-              Nome
-            </p>
-            <input type="text" />
-          </div>
-          <div className="input">
-            <p>
-              <Lock />
-              Senha
-            </p>
-            <input type="password" />
-          </div>
-          <div className="logIn">
-            <div className="social-media">
-              <ul className="list-social-media">
-                <a className="link-social-media" href="/#">
-                  <li className="item-social-media">
-                    <Facebook />
-                  </li>
-                </a>
-                <a className="link-social-media" href="/#">
-                  <li className="item-social-media">
-                    <Twitter />
-                  </li>
-                </a>
-                <a className="link-social-media" href="/#">
-                  <li className="item-social-media">
-                    <Google />
-                  </li>
-                </a>
-              </ul>
+          <div className="login-container">
+            <div className="change-input">
+              <p>Login</p>
+              <div></div>
+              <p>Cadastro</p>
             </div>
-            <button>
-              <ArrowRight />
-            </button>
+            <h1>Bem vindo de volta!</h1>
+            <p>
+              Digite o seu email e senha cadastrados e continue upando seus
+              arquivos com total segurança.
+            </p>
+            <h2>Login</h2>
+            <div className="input">
+              <p>
+                <User />
+                Nome
+              </p>
+              <input type="text" />
+            </div>
+            <div className="input">
+              <p>
+                <Lock />
+                Senha
+              </p>
+              <input type="password" />
+            </div>
+            <div className="logIn">
+              <div className="social-media">
+                <ul className="list-social-media">
+                  <a className="link-social-media" href="/#">
+                    <li className="item-social-media">
+                      <Facebook />
+                    </li>
+                  </a>
+                  <a className="link-social-media" href="/#">
+                    <li className="item-social-media">
+                      <Twitter />
+                    </li>
+                  </a>
+                  <a className="link-social-media" href="/#">
+                    <li className="item-social-media">
+                      <Google />
+                    </li>
+                  </a>
+                </ul>
+              </div>
+              <button>
+                <ArrowRight />
+              </button>
+            </div>
+            <p className="suggest">
+              Ainda não é cadastrado? <a>Criar conta</a>
+            </p>
           </div>
-          <p className="suggest">
-            Ainda não é cadastrado? <a>Criar conta</a>
-          </p>
           <p className="copyright">
             Copyright © All Rights Reserved - Intelligence
           </p>
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -8,9 +8,13 @@ export const Container = styled.div`
   align-items: center;
   flex-direction: column;
   h1 {
-    font-size: 3.375rem;
+    font-size: 1.7rem;
     color: ${props => props.theme.colors.primary};
-    margin-top: 40px;
+  }
+  h2 {
+    font-size: 1.3rem;
+    color: ${props => props.theme.colors.primary};
+    margin: 0px;
   }
   p {
     line-height: 32px;
@@ -18,12 +22,11 @@ export const Container = styled.div`
   main {
     width: 100vw;
     height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    display: grid;
+    grid-template-columns: 3fr 1.3fr;
     .first-panel {
       height: 100vh;
-      flex: 70%;
+
       display: flex;
       align-items: center;
       justify-content: center;
@@ -35,121 +38,143 @@ export const Container = styled.div`
     .auth-panel {
       box-shadow: -5px 3px 5px -3px rgba(0, 0, 0, 0.65);
       height: 100vh;
-      flex: 30%;
+      position: relative;
       background: #6666cc;
       display: flex;
       flex-direction: column;
+      justify-content: flex-end;
       align-items: center;
-      justify-content: space-around;
-      .change-input {
-        width: 100%;
+      .login-container {
         display: flex;
+        flex-direction: column;
+        height: 100%;
+        width: 100%;
         align-items: center;
-        justify-content: center;
-        div {
-          width: 2px;
-          height: 1.1rem;
-          background-color: #f2f2f2;
-          margin: 5px;
-        }
         p {
-          font-size: 1.1rem;
+          font-size: 0.8rem;
+          text-align: center;
+          line-height: 18px;
         }
-      }
-      .input {
-        width: 60%;
-        p {
+        h2 {
+          margin-top: 24px !important;
+        }
+        .change-input {
+          width: 100%;
           display: flex;
           align-items: center;
-          justify-content: first baseline;
-          font-size: 0.938rem;
-          font-weight: bold;
-          svg {
-            margin-right: 5px;
-            width: 18px;
-            height: 18px;
+          justify-content: center;
+          margin: 8px 0px;
+          margin-bottom: 35px;
+          div {
+            width: 2px;
+            height: 1.1rem;
+            background-color: #f2f2f2;
+            margin: 5px;
+          }
+          p {
+            font-size: 1.1rem;
           }
         }
-        input {
-          outline: none;
-          width: 100%;
-          height: 45px;
-          border-radius: 5px;
-          border: none;
-          padding-right: 20px;
-          padding-left: 20px;
-        }
-      }
-      .logIn {
-        width: 60%;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        .social-media {
-          .list-social-media {
-            list-style-type: none;
+        .input {
+          width: 60%;
+          margin: 8px 0px;
+          p {
             display: flex;
-            margin: 0;
-            padding: 0;
-            .item-social-media {
-              transition: all 0.2s ease-in;
-              border: 1px solid #bdc3c7;
-              border-radius: 50%;
-              width: 35px;
-              height: 35px;
+            align-items: center;
+            justify-content: first baseline;
+            font-size: 0.938rem;
+            font-weight: bold;
+            line-height: 32px;
+            svg {
+              margin-right: 5px;
+              width: 18px;
+              height: 18px;
+            }
+          }
+          input {
+            outline: none;
+            width: 100%;
+            height: 2.813rem;
+            border-radius: 5px;
+            border: none;
+            padding-right: 20px;
+            padding-left: 20px;
+          }
+        }
+        .logIn {
+          width: 60%;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          margin: 12px 0px;
+          .social-media {
+            .list-social-media {
+              list-style-type: none;
               display: flex;
-              justify-content: center;
-              align-items: center;
-              margin-left: 10px;
-              svg {
-                width: 60%;
-                color: ${props => props.theme.colors.primary};
-              }
-              &:hover {
-                background-color: #f2f2f2;
+              margin: 0;
+              padding: 0;
+              .item-social-media {
+                transition: all 0.2s ease-in;
+                border: 1px solid #bdc3c7;
+                border-radius: 50%;
+                width: 35px;
+                height: 35px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-left: 10px;
                 svg {
-                  color: #6666cc;
+                  width: 60%;
+                  color: ${props => props.theme.colors.primary};
+                }
+                &:hover {
+                  background-color: #f2f2f2;
+                  svg {
+                    color: #6666cc;
+                  }
                 }
               }
             }
           }
-        }
-        button {
-          transition: all 0.2s ease-in;
-          width: 80px;
-          height: 35px;
-          border-radius: 5px;
-          border: none;
-          outline: none;
-          background: transparent;
-          border: 1px solid #bdc3c7;
-          cursor: pointer;
-          svg {
-            color: #f2f2f2;
-            width: 80%;
-            height: 80%;
-          }
-          &:hover {
-            background-color: #f2f2f2;
+          button {
+            transition: all 0.2s ease-in;
+            width: 80px;
+            height: 35px;
+            border-radius: 5px;
+            border: none;
+            outline: none;
+            background: transparent;
+            border: 1px solid #bdc3c7;
+            cursor: pointer;
             svg {
-              color: #6666cc;
+              color: #f2f2f2;
+              width: 80%;
+              height: 80%;
+            }
+            &:hover {
+              background-color: #f2f2f2;
+              svg {
+                color: #6666cc;
+              }
+            }
+          }
+        }
+        .suggest {
+          font-size: 0.938rem;
+          a {
+            cursor: pointer;
+            color: #57b0c9;
+            transition: all 0.1s ease-in;
+            text-decoration: underline;
+            text-decoration-color: rgba(0, 0, 0, 0.15);
+            font-weight: bold;
+            &:hover {
+              text-decoration-color: #57b0c9;
             }
           }
         }
       }
-      .suggest {
-        font-size: 0.938rem;
-        a {
-          cursor: pointer;
-          color: #57b0c9;
-          transition: all 0.1s ease-in;
-          text-decoration: underline;
-          text-decoration-color: rgba(0, 0, 0, 0.15);
-          &:hover {
-            text-decoration-color: #57b0c9;
-          }
-        }
-      }
+
       .copyright {
         font-size: 0.75rem;
       }
