af6a9c4e99b19aaba917e6ff60e0ad1cda5c5a7c
JoseCarlos0056 <josecarlos.depaula17@gmail.com>
2021-01-28 23:47:18 -0300

    start template home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,15 +4,29 @@ import rocktseatLogo from '../assets/rocketseat.svg'
 import { Container } from '../styles/pages/Home'
 
 const Home: React.FC = () => {
+
   return (
     <Container>
       <Head>
         <title>Oben - Página Inicial</title>
 
       </Head>
-      <header></header>
+      <header>
+        <img src={rocktseatLogo} alt="oben logo" />
+        <a onClick={()=>{
+          window.location.href = '/'
+        }}>Sair</a>
+      </header>
       <main>
-       
+       <div></div>
+       <div></div>
+       <div></div>
+       <div></div>
+       <div></div>
+       <div></div>
+       <div></div>
+       <div></div>
+       <div></div>
       </main>
     </Container>
   )
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -5,7 +5,7 @@ export const Container = styled.div`
   height: 100vh;
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows:20vh 80vh;
+  grid-template-rows:10vh 90vh;
   h1 {
     font-size: 2em;
     color: ${props => props.theme.colors.primary};
@@ -13,19 +13,30 @@ export const Container = styled.div`
   header{
     /* width: 100%;
     height: 100px; */
-    background: green;
+    padding: 0px 20px;
+    background: #59599e;
    grid-column-start: 1;
    grid-column-end: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
   main {
-    background: brown;
-    /* width: 100%;
-    height: 100px; */
-    /* width: 100vw;
-    height: 100vh;
+    background: #f2f2f2;
     display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows:20vh 80vh */
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-gap: 20px;
+    justify-content: center;
+    align-items: center;
+    /* align-items: space-evenly;
+    justify-content: space-evenly; */
+    div{
+      align-self: center;
+    width: 100%;
+    height: 100%;
+    background: #59599e;
+    }
   
   }
 `
